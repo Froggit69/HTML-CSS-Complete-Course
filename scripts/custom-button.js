834d7237840da1a05c9129e8ab983cd956333ed0
Catgirl <zeroaninea@gmail.com>
2025-09-08 16:33:33 +0400

    comments added

--- scripts/custom-button.js
+++ scripts/custom-button.js
@@ -1,9 +1,10 @@
-const button = document.getElementById('custom-button');
+const button = document.getElementById('custom-button'); // Find the button by its ID.
 
 button.addEventListener('click', () => {
-  button.classList.add('clicked');
+  // If the button is clicked.
+  button.classList.add('clicked'); // Set the `clicked` class name.
 
   setTimeout(() => {
-    button.classList.remove('clicked');
+    button.classList.remove('clicked'); // Remove it after 100 miliseconds.
   }, 100);
 });
